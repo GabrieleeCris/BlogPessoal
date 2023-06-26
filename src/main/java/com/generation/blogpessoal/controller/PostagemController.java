f610ff6b8061f13b7cdf62af86b3b5865cb12a26
Gabriele <cristiine94@outlook.com>
2023-06-26 18:02:35 -0300

    Projeto guia de aprendizado-contrução

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -7,12 +7,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/postagens")
@@ -40,7 +44,11 @@ public class PostagemController {
 	public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo){
 		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
 	}
-	
+	@PostMapping 
+public ResponseEntity <Postagem> post(@Valid @RequestBody Postagem postagem){
+		return ResponseEntity.of(status(HttpStatus.ACCEPTED. CREATED))
+				.body(postagemRepository.save(postagem));
+	}
 	
 	
 	
