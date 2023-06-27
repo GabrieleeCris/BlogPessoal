a724c7984217026a1f00c5b7c57f783efda70cf6
Gabriele <cristiine94@outlook.com>
2023-06-27 17:18:14 -0300

    Projeto guia de aprendizado-construção

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -1,19 +1,27 @@
 package com.generation.blogpessoal.controller;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
+import com.generation.blogpessoal.repository.TemaRepository;
+
 import jakarta.validation.Valid;
 
 @RestController
@@ -24,6 +32,9 @@ public class PostagemController {
 	@Autowired
 	private PostagemRepository postagemRepository;
 	//
+	
+	@Autowired
+	private TemaRepository temaRepository;
 	
 	@GetMapping 
 	public ResponseEntity <List<Postagem>> getAll(){
@@ -44,10 +55,34 @@ public class PostagemController {
 	}
 	@PostMapping
 	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem) {
+		if(temaRepository.existsById(postagem.getTema().getId()))
 		return ResponseEntity.status(HttpStatus.CREATED)
 				.body(postagemRepository.save(postagem));
-	}
-	}
+		throw new ResponseStatusException(HttpStatus. BAD_REQUEST, "Tema não existe!", null );
+	
+    }
+	@PutMapping 
+	public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem){
+		if(postagemRepository.existsById(postagem.getId()))
+			if(temaRepository.existsById(postagem.getTema().getId()))
+
+		return ResponseEntity.status(HttpStatus.OK)
+				.body(postagemRepository.save(postagem));		
+		     throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Tema não existe!" , null);
+		     
+		}
+	    return new ResponseEntity.status(HttpStatus.NOT_FOUND). build();
+
+     @ResponseStatus(HttpStatus.NO_CONTENT)
+     @DeleteMapping("/{id}")
+       public void delete(@PathVariable Long id) {
+    	 Optional<Postagem> postagem = postagemRepository.findById(id);
+    	 if(postagem.isEmpty())
+    		 throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    	 postagemRepository.deleteById(id);
+    	 
+     }
+	return ResponseEntity.status(HttpStatus.NOT_FOUD).build();
 	
 	
 	
@@ -57,4 +92,8 @@ public class PostagemController {
 	
 	
 	
+
+}
+}
+
 
--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -3,10 +3,13 @@ package com.generation.blogpessoal.model;
 import java.time.LocalDateTime;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType; 
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -64,6 +67,26 @@ import jakarta.validation.constraints.Size;
 		private LocalDateTime data;
 		
 		
-	}
-	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
 
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+	  
+
+	
+	
+	
+	
+	
+	
+	
+	
+}
